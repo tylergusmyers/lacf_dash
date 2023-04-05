5a6363db62928099b46af1aa770a7d3fafb9f0a2
Tyler Myers <tylermyers@Tylers-MacBook-Air.local>
2023-04-05 16:21:25 -0700

    Push for csv read

--- lacf-dashboarder/src/App.jsx
+++ lacf-dashboarder/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import './lacf.csv' 
+import * as d3 from 'd3';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [val, setVal] = useState(0)
+  d3.csv(csvData, function(error, data) {
+    if (error) {
+      console.error('Error loading CSV data:', error);
+      return;
+    }
+    const limitedData = data.slice(0, 10);
+    console.log(limitedData);
+    const maxValue = d3.max(limitedData, function(d) {
+      return +d.value;
+    });
+
+    console.log('Max value:', maxValue);
+  });
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>hello {val}</div>  
     </div>
   )
 }
