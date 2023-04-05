dea494d26b6d1209aa8569bd10cd35a6cf51346e
Tyler Myers <tylermyers@Tylers-MacBook-Air.local>
2023-04-05 16:59:08 -0700

    First run of bar chart

--- lacf-dashboarder/src/App.jsx
+++ lacf-dashboarder/src/App.jsx
@@ -2,34 +2,69 @@ import { useState } from 'react'
 import './App.css'
 import './lacf.csv' 
 import Papa from 'papaparse';
+import * as d3 from 'd3';
 
-
+import { useState, useEffect } from 'react';
+import Papa from 'papaparse';
+import * as d3 from 'd3';
 
 function App() {
-  const [val, setVal] = useState(0)
-  Papa.parse(csvData, {
-    header: true,
-    complete: function(results) {
-      const data = results.data;
+  const [data, setData] = useState([]);
 
-      // Log the data to the console
-      console.log(data);
+  useEffect(() => {
+    Papa.parse('/path/to/your/csv/file', {
+      download: true,
+      header: true,
+      dynamicTyping: true,
+      preview: 10, // Set this to 10 to only load the first 10 rows
+      complete: results => {
+        setData(results.data);
+      }
+    });
+  }, []);
 
-      // Use PapaParse to manipulate the data
-      const maxValue = Papa.unparse(data).split('\n').map(row => row.split(',')[1]).slice(1).map(Number).reduce((a, b) => Math.max(a, b));
+  useEffect(() => {
+    const margin = { top: 20, right: 20, bottom: 30, left: 40 };
+    const width = 600 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
 
-      console.log('Max value:', maxValue);
-    },
-    error: function(error) {
-      console.error('Error loading CSV data:', error);
-    }
-  })
+    const svg = d3.select('#chart')
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    const x = d3.scaleBand()
+      .range([0, width])
+      .padding(0.1)
+      .domain(data.map(d => d.name));
+
+    const y = d3.scaleLinear()
+      .range([height, 0])
+      .domain([0, d3.max(data, d => d.value)]);
+
+    svg.append('g')
+      .attr('transform', `translate(0,${height})`)
+      .call(d3.axisBottom(x));
+
+    svg.append('g')
+      .call(d3.axisLeft(y));
+
+    svg.selectAll('.bar')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', d => x(d.name))
+      .attr('y', d => y(d.value))
+      .attr('width', x.bandwidth())
+      .attr('height', d => height - y(d.value));
+  }, [data]);
 
   return (
-    <div className="App">
-      <div>hello {val}</div>  
-    </div>
-  )
+    <div id="chart"></div>
+  );
 }
 
 export default App
